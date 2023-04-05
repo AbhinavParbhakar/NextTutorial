676d98466d087d69b5162d48e7b8c38ae6a7f20d
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-04-05 17:54:06 -0600

    added 404 page

---
+++ tutorial/pages/404.tsx
@@ -0,0 +1,10 @@
+import { Redirect } from "next/dist/lib/load-custom-routes"
+
+export default function errorPage(){
+    return(
+        <div>
+            <h1>Page not Found, Redirecting....</h1>
+            
+        </div>
+    )
+}
--- tutorial/pages/index.tsx
+++ tutorial/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home(){
     return (
         <div className="text-3xl font-bold underline">
         <h1>This is the home page</h1>
-        <h2><Link href="/apps" className="text-blue-300">books</Link></h2>
+        <h2><Link href="/apps" className="text-blue-300">Apps</Link></h2>
         </div>
     )
 }
